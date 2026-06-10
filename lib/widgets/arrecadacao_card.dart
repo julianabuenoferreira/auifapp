e289a0a224df01f46c20b3678c275dd229d659d4
julianabuenoferreira <julianabuenof4788@gmail.com>
2026-06-10 18:43:40 -0300

    Create arrecadacao_card.dart

---
+++ lib/widgets/arrecadacao_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ArrecadacaoCard extends StatelessWidget {
+
+  final String titulo;
+  final int arrecadado;
+  final int meta;
+  final Color cor;
+
+  const ArrecadacaoCard({
+    super.key,
+    required this.titulo,
+    required this.arrecadado,
+    required this.meta,
+    required this.cor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      margin: const EdgeInsets.all(15),
+
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+
+        child: Column(
+          children: [
+
+            Text(
+              titulo,
+              style: TextStyle(
+                fontSize: 22,
+                color: cor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Text('Meta: $meta'),
+
+            Text('Arrecadado: $arrecadado'),
+
+            LinearProgressIndicator(
+              value: arrecadado / meta,
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
