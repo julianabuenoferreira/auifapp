3718efd5164804313fb13d9cc7945bb7647217c1
julianabuenoferreira <julianabuenof4788@gmail.com>
2026-06-10 18:37:18 -0300

    Create cachorros_data.dart

---
+++ lib/data/cachorros_data.dart
@@ -0,0 +1,19 @@
+import '../models/cachorro.dart';
+
+final List<Cachorro> cachorros = [
+
+  Cachorro(
+    nome: 'Chica',
+    descricao: 'Fêmea bronze muito carinhosa.',
+    saude: 'Vacinas em dia.',
+    imagem: 'assets/images/chica.jpg',
+  ),
+
+  Cachorro(
+    nome: 'Tibúrcio',
+    descricao: 'Cachorro grande amarelo.',
+    saude: 'Necessita de mais ração.',
+    imagem: 'assets/images/tiburcio.jpg',
+  ),
+
+];
